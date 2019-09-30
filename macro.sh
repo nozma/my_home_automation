4a08e238773f1a3e0d728308f5969edc4b2e20bc
nozma <co.panda.fc3s[at]gmail.com>
2019-09-30 23:53:38 +0900

    調光を細かく

--- macro.sh
+++ macro.sh
@@ -1,31 +1,46 @@
 #!/bin/bash
 
-source ./env.sh
+AUTO_HOME=/home/pi/auto
 
 case $1 in
-
-  light1)
-    ${AUTO_HOME}/curl.sh hue_1
-    ${AUTO_HOME}/curl.sh light_off
-    ;;
-
-  light2)
-    ${AUTO_HOME}/curl.sh hue_2
-    ${AUTO_HOME}/curl.sh light_off
-    ;;
-
-  light3)
-    ${AUTO_HOME}/curl.sh hue_off
-    ${AUTO_HOME}/curl.sh light_on
-    ;;
-
-  light4)
-    ${AUTO_HOME}/curl.sh hue_2
-    ${AUTO_HOME}/curl.sh light_on
-    ;;
 
   light_off)
     ${AUTO_HOME}/curl.sh hue_off
     ${AUTO_HOME}/curl.sh light_off
+    ;;
+
+  light10)
+    ${AUTO_HOME}/curl.sh hue_1
+    ${AUTO_HOME}/curl.sh light_off
+    ;;
+
+  light20)
+    ${AUTO_HOME}/curl.sh hue_1
+    ${AUTO_HOME}/curl.sh light_on
+    ${AUTO_HOME}/set_light.sh 20
+    ;;
+
+  light40)
+    ${AUTO_HOME}/curl.sh hue_1
+    ${AUTO_HOME}/curl.sh light_on
+    ${AUTO_HOME}/set_light.sh 40
+    ;;
+
+  light60)
+    ${AUTO_HOME}/curl.sh hue_2
+    ${AUTO_HOME}/curl.sh light_on
+    ${AUTO_HOME}/set_light.sh 60
+    ;;
+
+  light80)
+    ${AUTO_HOME}/curl.sh hue_2
+    ${AUTO_HOME}/curl.sh light_on
+    ${AUTO_HOME}/set_light.sh 80
+    ;;
+
+  light100)
+    ${AUTO_HOME}/curl.sh hue_2
+    ${AUTO_HOME}/curl.sh light_on
+    ${AUTO_HOME}/set_light.sh 100
     ;;
 esac
