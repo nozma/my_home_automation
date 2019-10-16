1fd753e8a4e007dd7b25ad24d1b5be7142abbf3a
nozma <co.panda.fc3s[at]gmail.com>
2019-10-16 20:11:54 +0900

    起床と就寝のマクロを追加

--- macro.sh
+++ macro.sh
@@ -43,4 +43,48 @@ case $1 in
     ${AUTO_HOME}/curl.sh light_on
     ${AUTO_HOME}/set_light.sh 100
     ;;
+
+  morning1)
+    ${AUTO_HOME}/curl.sh hue_morning_1
+    ;;
+
+  morning2)
+    ${AUTO_HOME}/curl.sh hue_morning_2
+    ;; 
+
+  morning3)
+    ${AUTO_HOME}/curl.sh hue_morning_3
+    ${AUTO_HOME}/curl.sh light_on
+    ${AUTO_HOME}/set_light.sh 20
+    ;; 
+
+  morning4)
+    ${AUTO_HOME}/set_light.sh 40
+    ;; 
+
+  morning5)
+    ${AUTO_HOME}/set_light.sh 100
+    ;; 
+
+  night1)
+    ${AUTO_HOME}/set_light.sh 80
+    ${AUTO_HOME}/curl.sh hue_night_3
+    ;;
+  
+  night2)
+    ${AUTO_HOME}/set_light.sh 40
+    ;;
+  
+  night3)
+    ${AUTO_HOME}/curl.sh light_off
+    ;;
+
+  night4)
+    ${AUTO_HOME}/curl.sh hue_night_2
+    ;;
+  
+  night5)
+    ${AUTO_HOME}/curl.sh hue_night_1
+    ;;
+
 esac
